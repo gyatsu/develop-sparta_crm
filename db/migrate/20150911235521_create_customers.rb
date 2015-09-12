1498f1a38f5969843928203068c5f9a66d1f7692
MR.Y <programming@yatsukouji.com>
2015-09-12 09:16:22 +0900

    email をmailに修正

--- db/migrate/20150911235521_create_customers.rb
+++ db/migrate/20150911235521_create_customers.rb
@@ -1,12 +1,13 @@
 class CreateCustomers < ActiveRecord::Migration
   def change
     create_table :customers do |t|
-      t.string :family_name, null: false, length { maximum: 20 }
-      t.string :given_name, null: false, length { maximum: 20 }
+      t.string :family_name, null: false, length: { maximum: 20 }
+      t.string :given_name, null: false, length: { maximum: 20 }
       t.string :mail, null: false
 
       t.timestamps null: false
     end
-  add_index :customers, :email, unique: true
+  add_index :customers, :mail, unique: true
+
   end
 end
