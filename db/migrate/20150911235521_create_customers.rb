de5bb3bf6dbe50b97889578d9c7d434d7a83278f
MR.Y <programming@yatsukouji.com>
2015-09-12 09:05:18 +0900

    length { maximum: 20 } を追加

--- db/migrate/20150911235521_create_customers.rb
+++ db/migrate/20150911235521_create_customers.rb
@@ -1,8 +1,8 @@
 class CreateCustomers < ActiveRecord::Migration
   def change
     create_table :customers do |t|
-      t.string :family_name, null: false
-      t.string :given_name, null: false
+      t.string :family_name, null: false, length { maximum: 20 }
+      t.string :given_name, null: false, length { maximum: 20 }
       t.string :mail, null: false
 
       t.timestamps null: false
