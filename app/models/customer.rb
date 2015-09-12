eefa22be058863a77301c773088a6ff66423c370
MR.Y <programming@yatsukouji.com>
2015-09-12 11:07:57 +0900

    中級以上？

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,6 @@
 class Customer < ActiveRecord::Base
-  def full_name
-    full_name = family_name + given_name + "様"
+  def full_name(keisho)
+    full_name = family_name + given_name + keisho
     return full_name
   end
 end
