6c3e56107ba0dac744a3c3d6a71d0da84eedce73
MR.Y <programming@yatsukouji.com>
2015-09-13 17:48:10 +0900

    rails g migration AddImageUrlToUsers image_url:stringとrake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150913063711) do
+ActiveRecord::Schema.define(version: 20150913084635) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 20150913063711) do
     t.datetime "updated_at",                          null: false
     t.string   "family_name"
     t.string   "given_name"
+    t.string   "image_url"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
